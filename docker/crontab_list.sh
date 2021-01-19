eee6abbf4468db3ab8914ba005bb9b600f45d1c8
lxk0301 <778732665@qq.com>
2021-01-19 18:40:45 +0800

    新增 宠汪汪为好友单独喂食一次的任务

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -26,7 +26,7 @@
 # 宠汪汪积分兑换奖品
 0 0-16/8 * * * node /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
 # 宠汪汪偷好友积分与狗粮
-0 0,6 * * * node /scripts/jd_joy_steal.js >> /scripts/logs/jd_joy_steal.log 2>&1
+0 0-10/2 * * * node /scripts/jd_joy_steal.js >> /scripts/logs/jd_joy_steal.log 2>&1
 # 摇钱树
 0 */2 * * * node /scripts/jd_moneyTree.js >> /scripts/logs/jd_moneyTree.log 2>&1
 # 东东萌宠
