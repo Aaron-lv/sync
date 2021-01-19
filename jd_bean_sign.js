8c6042729444f5208b2c9f45a1a6c5dce518cdc1
lxk0301 <778732665@qq.com>
2021-01-19 14:38:33 +0800

    update

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -124,7 +124,20 @@ async function downFile () {
     url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js';
   }
   try {
-    await download(url, outPutUrl);
+    const options = {}
+    if (process.env.TG_PROXY_HOST && process.env.TG_PROXY_PORT) {
+      const tunnel = require("tunnel");
+      const agent = {
+        https: tunnel.httpsOverHttp({
+          proxy: {
+            host: process.env.TG_PROXY_HOST,
+            port: process.env.TG_PROXY_PORT * 1
+          }
+        })
+      }
+      Object.assign(options, { agent })
+    }
+    await download(url, outPutUrl, options);
     console.log('文件下载完毕');
   } catch (e) {
     console.log("文件下载异常:" + e);
