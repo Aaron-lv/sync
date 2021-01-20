a6fa7b17117704424c0a37d7bf1864d92a96662e
LXK9301 <72646373+LXK9301@users.noreply.github.com>
2021-01-20 22:44:09 +0800

    Update jd_rankingList.js

--- jd_rankingList.js
+++ jd_rankingList.js
@@ -1,21 +1,10 @@
 /*
-自用于github action
+
+京东排行榜
+
+自用
 author：yangtingxiao
 github： https://github.com/yangtingxiao
-京东排行榜
-更新时间：2020-11-05 16:07
-脚本说明：京东排行榜签到得京豆
-活动入口：找不着了，点击脚本通知进入吧
-脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
-// quantumultx
-[task_local]
-#京东排行榜
-11 9 * * * https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/jd_rankingList.js, tag=京东排行榜, img-url=https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/image/jd.png, enabled=true
-// Loon
-[Script]
-cron "11 9 * * *" script-path=https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/jd_rankingList.js,tag=京东排行榜
-// Surge
-京东排行榜 = type=cron,cronexp=11 9 * * *,wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/jd_rankingList.js
  */
 const $ = new Env('京东排行榜');
 main();
