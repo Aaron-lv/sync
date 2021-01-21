dad1aa80957889cd32cd0b8ccad0c4db996ccb50
lxk0301 <778732665@qq.com>
2021-01-21 11:19:08 +0800

    update

--- jd_petTreasureBox.js
+++ jd_petTreasureBox.js
@@ -1,6 +1,6 @@
-/**
- 更新时间:2020-11-12
- 从github@Zero-S1搬的[https://github.com/Zero-S1/JD_tools/blob/master/jbp.js]
+/*
+更新时间:2020-11-12
+从github@Zero-S1搬的[https://github.com/Zero-S1/JD_tools/blob/master/jbp.js]
 【宠汪汪聚宝盆辅助脚本】
 1、进入聚宝盆,显示本轮狗粮池投入总数,方便估算
 2、可能有两位数误差,影响不大
@@ -8,19 +8,19 @@
 [MITM]
 hostname = jdjoy.jd.com,draw.jdfcloud.com
 
-surge
+==========Surge=============
 [Script]
 聚宝盆投狗粮辅助 = type=http-response,pattern=^https:\/\/jdjoy\.jd\.com\/pet\/getPetTreasureBox|^https:\/\/draw\.jdfcloud\.com\/\/pet\/getPetTreasureBox,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_petTreasureBox.js
 
-Qx
+===================Quantumult X=====================
 [rewrite_local]
 ^https:\/\/jdjoy\.jd\.com\/pet\/getPetTreasureBox|^https:\/\/draw\.jdfcloud\.com\/\/pet\/getPetTreasureBox url script-response-body https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_petTreasureBox.js
 
-LOON：
+=====================Loon=====================
 [Script]
 http-response ^https:\/\/jdjoy\.jd\.com\/pet\/getPetTreasureBox|^https:\/\/draw\.jdfcloud\.com\/\/pet\/getPetTreasureBox script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_petTreasureBox.js, requires-body=true, timeout=10, tag=聚宝盆投狗粮辅助
 
-**/
+*/
 let body = $response.body
 body = JSON.parse(body)
 food = body['data']['food']
