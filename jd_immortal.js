3bb7837d1c29c1232a67aa427a3a0130aa5d46d2
anonymous <anonymous@gmail.com>
2021-01-23 23:12:46 +0800

    debug

--- jd_immortal.js
+++ jd_immortal.js
@@ -311,7 +311,7 @@ function shareCodesFormat() {
       const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
       $.newShareCodes = inviteCodes[tempIndex].split('@');
     }
-    const readShareCodeRes = [] //await readShareCode();
+    const readShareCodeRes = await readShareCode();
     if (readShareCodeRes && readShareCodeRes.code === 200) {
       $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
     }
