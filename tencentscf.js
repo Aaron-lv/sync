c2bdb332b4c55b403378649d01499832a9a247f2
any <any@gmail.com>
2021-01-25 22:02:44 +0800

    更新环境变量与触发器

--- tencentscf.js
+++ tencentscf.js
@@ -20,7 +20,7 @@ const clientConfig = {
 };
 
 const client = new ScfClient(clientConfig);
-const params = {
+let params = {
   "Handler": "index.main_handler",
   "FunctionName": process.env.TENCENT_FUNCTION_NAME, // 云函数程序名，例如 jd_scripts
   "ZipFile": contents_in_base64
@@ -33,3 +33,47 @@ client.UpdateFunctionCode(params).then(
     console.error("error", err);
   }
 );
+
+// 更新环境变量
+let vars = []
+for(let key in process.env){
+  vars.push({
+    "Key": key,
+    "Value": process.env[key]
+  })
+}
+
+params = {
+  "Environment": {
+    "Variables": vars
+  }
+};
+client.UpdateFunctionConfiguration(params).then(
+  (data) => {
+    console.log(data);
+  },
+  (err) => {
+    console.error("error", err);
+  }
+);
+
+// 更新触发器
+const inputYML = 'serverless.yml';
+const yaml = require('js-yaml');
+const obj = yaml.load(fs.readFileSync(inputYML, {encoding: 'utf-8'}))
+for(let vo of obj.inputs.events){
+  let param = {
+    'Type' : "timer",
+    'TriggerDesc' : vo.timer.parameters.cronExpression,
+    'CustomArgument' : vo.timer.parameters.argument,
+    'Enable' : "true"
+  }
+  client.CreateTrigger(param).then(
+    (data) => {
+      console.log(data);
+    },
+    (err) => {
+      console.error("error", err);
+    }
+  );
+}
