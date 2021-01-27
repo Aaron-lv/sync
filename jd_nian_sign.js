206d678ea0a755f8bfe1bf28365d8cd0e02ae798
any <any@email.com>
2021-01-27 10:47:52 +0800

    自动将活动入口列在Readme脚本清单中，识别每个脚本中“活动入口：”这一行全角冒号后面的内容作为入口内容。

--- jd_nian_sign.js
+++ jd_nian_sign.js
@@ -2,8 +2,8 @@
 京东炸年兽签到任务🧨
 活动时间:2021-1-18至2021-2-11
 暂不加入品牌会员
-活动入口:https://wbbny.m.jd.com/babelDiy/Zeus/2cKMj86srRdhgWcKonfExzK4ZMBy/index.html
-活动地址：京东app左侧浮动窗口
+地址：https://wbbny.m.jd.com/babelDiy/Zeus/2cKMj86srRdhgWcKonfExzK4ZMBy/index.html
+活动入口：京东app左侧浮动窗口
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 ============Quantumultx===============
