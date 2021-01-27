69d18f2386e3b805e6851a577a840456c88d56c7
any <any@email.com>
2021-01-27 10:47:52 +0800

    自动将活动入口列在Readme脚本清单中，识别每个脚本中“活动入口：”这一行全角冒号后面的内容作为入口内容。

--- jd_fruit.js
+++ jd_fruit.js
@@ -1,6 +1,7 @@
 /*
 东东水果:脚本更新地址 https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_fruit.js
 更新时间：2021-1-9
+活动入口：京东APP我的-更多工具-东东农场
 东东农场活动链接：https://h5.m.jd.com/babelDiy/Zeus/3KSjXqQabiTuD1cJ28QskrpWoBKT/index.html
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
