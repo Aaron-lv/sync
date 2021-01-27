0e10c97c44afdf81ae4c92b413ed5671de03519d
EvineDeng <evinedeng@foxmail.com>
2021-01-27 10:47:52 +0800

    自动将活动入口列在Readme脚本清单中，识别每个脚本中“活动入口：”这一行全角冒号后面的内容作为入口内容。

--- jd_speed.js
+++ jd_speed.js
@@ -1,6 +1,7 @@
 /*
 京东天天加速链接：https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_speed.js
 更新时间：2020-12-25
+活动入口：京东APP我的-更多工具-天天加速
 支持京东双账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 每天4京豆，再小的苍蝇也是肉
