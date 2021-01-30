00204bfee730ee208cccc00208777693f4bd902d
lxk0301 <778732665@qq.com>
2021-01-30 18:44:21 +0800

    把狂欢城写进docker里面，多次运行增大被助力到的几率

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -20,7 +20,7 @@
 5 1 * * * node /scripts/jd_immortal_answer.js >> /scripts/logs/jd_immortal_answer.log 2>&1
 # 5G狂欢城
 0 */6 * * * node /scripts/jd_5g.js >> /scripts/logs/jd_5g.log 2>&1
-
+0 */6 * * * node /scripts/jd_818.js >> /scripts/logs/jd_818.log 2>&1
 
 ##############长期活动##############
 # 签到
