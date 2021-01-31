1fd2eaa9068706a26a71cdceda2bd022caf1307d
lxk0301 <778732665@qq.com>
2021-01-31 21:07:51 +0800

    debug

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -1389,6 +1389,8 @@ function requireConfig() {
           $.shareCodesArr.push(shareCodes[item])
         }
       })
+    } else {
+      console.log(`惊喜工厂邀请码:${$.getdata('jd_jxFactory')}`)
     }
     // console.log(`\n种豆得豆助力码::${JSON.stringify($.shareCodesArr)}`);
     console.log(`您提供了${$.shareCodesArr.length}个账号的${$.name}助力码\n`);
