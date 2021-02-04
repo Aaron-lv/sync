1304e30f7187a7d11d4b5371c3624eb74fba72b0
lxk0301 <778732665@qq.com>
2021-02-04 09:15:29 +0800

    update

--- jd_818.js
+++ jd_818.js
@@ -55,7 +55,7 @@ if ($.isNode()) {
 }
 
 const JD_API_HOST = 'https://rdcseason.m.jd.com/api/';
-const activeEndTime = '2021/2/4 23:59:59+08:00';
+const activeEndTime = '2021/2/4 00:59:59+08:00';
 const addUrl = 'http://jd.turinglabs.net/helpcode/create/';
 const printUrl = `http://jd.turinglabs.net/api/v2/jd/5g/read/30/`;
 let helpCode = []
