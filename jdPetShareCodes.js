218cd6ccccc8a7566d80a60304df21fb18539791
lxk0301 <778732665@qq.com>
2021-02-04 15:39:19 +0800

    修改文字明说

--- jdPetShareCodes.js
+++ jdPetShareCodes.js
@@ -22,7 +22,7 @@ if (process.env.PETSHARECODES) {
     PetShareCodes = process.env.PETSHARECODES.split();
   }
 } else if (process.env.JD_COOKIE) {
-  console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+  console.log(`由于您环境变量(PETSHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
 }
 for (let i = 0; i < PetShareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
