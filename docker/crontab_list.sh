e9df9533cd29ee1dc4801e2869e38973f1de0f37
lxk0301 <778732665@qq.com>
2021-02-06 00:52:01 +0800

    crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,10 +4,6 @@
 ##############短期活动##############
 # 年货节(活动时间：2021年1月9日-2021年2月9日)
 10 8 * * * node /scripts/jd_nh.js >> /scripts/logs/jd_nh.log 2>&1
-# 京东神仙书院(活动时间:2021-1-20至2021-2-5)
-30 6,8,12,22 * * * node /scripts/jd_immortal.js >> /scripts/logs/jd_immortal.log 2>&1
-# 京东神仙书院答题(活动时间:2021-1-20至2021-2-5)
-5 1 * * * node /scripts/jd_immortal_answer.js >> /scripts/logs/jd_immortal_answer.log 2>&1
 
 # 小鸽有礼(活动时间：2021年1月15日至2021年2月19日)
 5 7 * * * node /scripts/jd_xg.js >> /scripts/logs/jd_xg.log 2>&1
@@ -15,8 +11,7 @@
 34 9 * * * node /scripts/jd_xgyl.js >> /scripts/logs/jd_jd_xgyl.log 2>&1
 # 京东手机年终奖(活动时间：2021年1月26日～2021年2月8日)
 15 0 * * * node /scripts/jd_festival.js >> /scripts/logs/jd_festival.log 2>&1
-# 盲盒抽京豆(活动时间：2021年1月6日～2021年2月5日)
-1 7 * * * node /scripts/jd_mh.js >> /scripts/logs/jd_mh.log 2>&1
+
 # 京东压岁钱(活动时间：2021-2-1至2021-2-11)
 20 8,12 * * * node /scripts/jd_newYearMoney.js >> /scripts/logs/jd_newYearMoney.log 2>&1
 # 京东压岁钱抢百元卡(活动时间：2021-2-1至2021-2-11)
