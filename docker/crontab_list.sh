a3c91d7baf70a13b86bc4745fdb1b6e0734fa6dc
lxk0301 <778732665@qq.com>
2021-02-06 10:20:39 +0800

    迁移过期活动(jd_festival.js&jd_super_box.js )&fix

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -9,9 +9,6 @@
 5 7 * * * node /scripts/jd_xg.js >> /scripts/logs/jd_xg.log 2>&1
 # 小鸽有礼2(活动时间：2021年1月28日～2021年2月28日)
 34 9 * * * node /scripts/jd_xgyl.js >> /scripts/logs/jd_jd_xgyl.log 2>&1
-# 京东手机年终奖(活动时间：2021年1月26日～2021年2月8日)
-15 0 * * * node /scripts/jd_festival.js >> /scripts/logs/jd_festival.log 2>&1
-
 # 京东压岁钱(活动时间：2021-2-1至2021-2-11)
 20 8,12 * * * node /scripts/jd_newYearMoney.js >> /scripts/logs/jd_newYearMoney.log 2>&1
 # 京东压岁钱抢百元卡(活动时间：2021-2-1至2021-2-11)
@@ -105,7 +102,5 @@
 27 8 * * * node /scripts/jd_sgmh.js >> /scripts/logs/jd_sgmh.log 2>&1
 # 京东秒秒币
 10 7 * * * node /scripts/jd_ms.js >> /scripts/logs/jd_ms.log 2>&1
-# 京东超级盒子
-20 7 * * * node /scripts/jd_super_box.js >> /scripts/logs/jd_super_box.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
