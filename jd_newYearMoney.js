192e0c5c7cffb181c9f223f7dce87ff4d8be881d
lxk0301 <778732665@qq.com>
2021-02-07 14:47:16 +0800

    修复宠汪汪强制助力失效的问题

--- jd_newYearMoney.js
+++ jd_newYearMoney.js
@@ -49,8 +49,8 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const inviteCodes = [
-  `jsttArEd_tgCAeE4Nvw8s1ZH9MxOC3gc1xJbHWjXpU2Q@oMZeKYUO_fgiVLBla-crs5_32pyv5bOM_W4Jprpv02EXVM0@oMZeXOZM8YkCBrczP-F2-Ru-IjBHfK31MgkFDXR7zPWSNhoj`,
-  `jsttArEd_tgCAeE4Nvw8s1ZH9MxOC3gc1xJbHWjXpU2Q@oMZeXOZM8YkCBrczP-F2-Ru-IjBHfK31MgkFDXR7zPWSNhoj`,
+  `oMZeKYUO_fgiVLBla-crs5_32pyv5bOM_W4Jprpv02EXVM0@oMZeXOZM8YkCBrczP-F2-Ru-IjBHfK31MgkFDXR7zPWSNhoj@oMZeXO9NqYkGUuA0NrJ1qy5TYAjfffsUrr73UVxt1z4SIB6j`,
+  `oMZeXOZM8YkCBrczP-F2-Ru-IjBHfK31MgkFDXR7zPWSNhoj@oMZeXO9NqYkGUuA0NrJ1qy5TYAjfffsUrr73UVxt1z4SIB6j`,
 ];
 !(async () => {
   await requireConfig();
