8e3c742213f04922dc68fe9a0ad1c02fd5f65f07
lxk0301 <778732665@qq.com>
2021-02-12 08:32:57 +0800

    删除过期脚本

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -6,8 +6,7 @@
 5 7 * * * node /scripts/jd_xg.js >> /scripts/logs/jd_xg.log 2>&1
 # 小鸽有礼2(活动时间：2021年1月28日～2021年2月28日)
 34 9 * * * node /scripts/jd_xgyl.js >> /scripts/logs/jd_jd_xgyl.log 2>&1
-# 京东压岁钱(活动时间：2021-2-1至2021-2-11)
-20 * 1-11 2 * node /scripts/jd_newYearMoney.js >> /scripts/logs/jd_newYearMoney.log 2>&1
+
 #环球挑战赛 活动时间：2021-02-02 至 2021-02-22
 0 9,12,20,21 2-22 2 * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
 
