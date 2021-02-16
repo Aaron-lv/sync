5fbca3b78a1152bacd19505b02bc5fc92935045d
lxk0301 <778732665@qq.com>
2021-02-16 09:33:56 +0800

    crontab_list.sh
    删除过期脚本

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -10,7 +10,6 @@
 #环球挑战赛 活动时间：2021-02-02 至 2021-02-22
 0 9,12,20,21 2-22 2 * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
 
-0 9,12,20,21 2-15 2 * node /scripts/jd_global_mh.js >> /scripts/logs/jd_global_mh.log 2>&1
 
 ##############长期活动##############
 # 签到
