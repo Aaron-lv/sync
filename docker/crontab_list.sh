10ec24f150ec0e15c6029703c43dc2cef06e2879
lxk0301 <778732665@qq.com>
2021-02-19 14:19:26 +0800

    迁移过期脚本，京东直播 jd_live.js

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -69,8 +69,6 @@
 0 0 * * * node /scripts/jd_car.js >> /scripts/logs/jd_car.log 2>&1
 # 领京豆额外奖励(每日可获得3京豆)
 33 4 * * * node /scripts/jd_bean_home.js >> /scripts/logs/jd_bean_home.log 2>&1
-# 京东直播(每日18豆)
-10-20/5 11 * * * node /scripts/jd_live.js >> /scripts/logs/jd_live.log 2>&1
 # 微信小程序京东赚赚
 10 11 * * * node /scripts/jd_jdzz.js >> /scripts/logs/jd_jdzz.log 2>&1
 # 宠汪汪邀请助力
