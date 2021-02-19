bd103fb53f5c302e0a01d0290ba7fbb757271220
lxk0301 <778732665@qq.com>
2021-02-19 11:37:25 +0800

    女装盲盒

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -10,7 +10,8 @@
 #环球挑战赛 活动时间：2021-02-02 至 2021-02-22
 0 9,12,20,21 2-22 2 * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
 
-
+#女装盲盒 活动时间：2021-2-19至2021-2-25
+5 7 19-25 2 * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 ##############长期活动##############
 # 签到
 3 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
