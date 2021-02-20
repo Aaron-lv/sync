390ccd9afbcf02827f15bbf9492cc737533d7b96
lxk0301 <778732665@qq.com>
2021-02-20 18:35:32 +0800

    新增京东保价，修改自 https://raw.githubusercontent.com/id77/QuantumultX/master/task/jdGuaranteedPrice.js

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -93,5 +93,7 @@
 10 7 * * * node /scripts/jd_ms.js >> /scripts/logs/jd_ms.log 2>&1
 #美丽研究院
 1 7,12,19 * * * node /scripts/jd_beauty.js >> /scripts/logs/jd_beauty.log 2>&1
+#京东保价
+1 0,23 * * * node /scripts/jd_price.js >> /scripts/logs/jd_price.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
