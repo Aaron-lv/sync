9ca4fdc6e059494673d68275edc664501b9378d6
anonymous <anonymous@gmail.com>
2021-02-21 14:01:32 +0800

    多了个回车

--- docker/default_task.sh
+++ docker/default_task.sh
@@ -106,7 +106,7 @@ fi
 random_m=$(($RANDOM % 60))
 
 echo "设定 docker_entrypoint.sh cron为："
-echo -e "\n# 必须要的默认定时任务请勿删除\n" >> $mergedListFile
+echo -e "\n# 必须要的默认定时任务请勿删除" >> $mergedListFile
 echo -e "${random_m} ${random_h} * * * docker_entrypoint.sh >> /scripts/logs/default_task.log 2>&1" | tee -a $mergedListFile
 
 
