da167a656308625069da28254d77c6a3c2d79e2c
lxk0301 <778732665@qq.com>
2021-02-23 11:25:00 +0800

    update docker

--- docker/notify_docker_user.js
+++ docker/notify_docker_user.js
@@ -1,9 +1,23 @@
 const notify = require('../sendNotify');
-
-function image_update_notify() {
-    if (process.env.NOTIFY_CONTENT) {
-        notify.sendNotify("⚠️Docker镜像版本更新通知⚠️", process.env.NOTIFY_CONTENT)
+const fs = require('fs');
+const notifyPath = '/scripts/logs/notify.txt';
+async function image_update_notify() {
+  if (fs.existsSync(notifyPath)) {
+    //notify.txt文件存在
+    const content = await fs.readFileSync(`${notifyPath}`, 'utf8');//读取notify.txt内容
+    if (!content.match(process.env.NOTIFY_CONTENT) && process.env.NOTIFY_CONTENT) {
+      await notify.sendNotify("⚠️Docker镜像版本更新通知⚠️", process.env.NOTIFY_CONTENT);
+      //把通知内容写入notify.txt文件
+      await fs.writeFileSync(`${notifyPath}`, JSON.stringify(process.env.NOTIFY_CONTENT));
     }
+  } else {
+    if (process.env.NOTIFY_CONTENT) {
+      notify.sendNotify("⚠️Docker镜像版本更新通知⚠️", process.env.NOTIFY_CONTENT)
+      //把通知内容写入notify.txt文件
+      await fs.writeFileSync(`${notifyPath}`, JSON.stringify(process.env.NOTIFY_CONTENT));
+    }
+  }
 }
-
-image_update_notify();
+!(async() => {
+  await image_update_notify();
+})().catch((e) => console.log(e))
