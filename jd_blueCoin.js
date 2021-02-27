c3a4b8cc58cc41b0c4f320b0e896eef110230342
lxk0301 <778732665@qq.com>
2021-02-27 16:07:02 +0800

    update jd_necklace.js ,云端多账号内容全集中到一次通知,注:部分推送方式可能会对内容有长度限制可能会导致推送失败或者推送内容不完整

--- jd_blueCoin.js
+++ jd_blueCoin.js
@@ -1,7 +1,7 @@
 /*
 东东超市兑换奖品 脚本地址：https://gitee.com/lxk0301/jd_scripts/raw/master/jd_blueCoin.js
 感谢@yangtingxiao提供PR
-更新时间：2020-12-24
+更新时间：2021-2-27
 活动入口：京东APP我的-更多工具-东东超市
 支持京东多个账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
