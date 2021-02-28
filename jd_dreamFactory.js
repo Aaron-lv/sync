52467bfacd287c644d99d72440efa51c8bcab386
lxk0301 <778732665@qq.com>
2021-02-28 13:43:05 +0800

    jd_dreamFactory.js update

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -117,7 +117,7 @@ async function jdDreamFactory() {
   try {
     await userInfo();
     await QueryFriendList();//查询今日招工情况以及剩余助力次数
-    await joinLeaderTuan();//参团
+    //await joinLeaderTuan();//参团
     await helpFriends();
     if (!$.unActive) return
     await getUserElectricity();
