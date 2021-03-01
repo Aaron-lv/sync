2fe45fc3e3b16a6363cbfc71a3af1bed890920ee
lxk0301 <778732665@qq.com>
2021-03-01 21:38:13 +0800

    jd_get_share_code.js update

--- jd_get_share_code.js
+++ jd_get_share_code.js
@@ -604,7 +604,7 @@ function getCFD(showInvite = true) {
           strMyShareId,
           strPin,
         } = JSON.parse(data);
-        console.log(`【账号${$.index}（${$.nickName || $.UserName}）财富岛】${strMyShareId}`)
+        console.log(`【账号${$.index}（${$.nickName || $.UserName}）财富岛】${strMyShareId}(每次都变化,不影响)`)
       } catch (e) {
         $.logErr(e, resp);
       } finally {
