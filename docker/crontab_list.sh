1bc2647a7beb810ca516389448c6adae73292128
lxk0301 <778732665@qq.com>
2021-03-01 12:07:50 +0800

    她的节，享京豆,一天700积分，两天1400积分换35京豆，一共8天，可换140京豆，差不多一期种豆得豆

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -2,8 +2,8 @@
 50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecode' | xargs rm -rf
 
 ##############短期活动##############
-# 小鸽有礼2(活动时间：2021年1月28日～2021年2月28日)
-34 9 * * * node /scripts/jd_xgyl.js >> /scripts/logs/jd_jd_xgyl.log 2>&1
+# 她的节，享京豆(活动共8天)
+0 8,21 1-8/1 3 * node /scripts/jd_firecrackers.js >> /scripts/logs/jd_firecrackers.log 2>&1
 
 #女装盲盒 活动时间：2021-2-26至2021-3-8
 5 0,7,23 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
