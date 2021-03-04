3e2ba5126b3aa7cd98b332555b4efb25ea3024fd
lxk0301 <778732665@qq.com>
2021-03-04 08:36:58 +0800

    crontab_list.sh update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -2,16 +2,15 @@
 50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecode' | xargs rm -rf
 
 ##############短期活动##############
-# 她的节，享京豆(活动共8天)
+# 她的节，享京豆(活动共8天，3.8日结束)
 0 8,21 1-8/1 3 * node /scripts/jd_firecrackers.js >> /scripts/logs/jd_firecrackers.log 2>&1
 
 #女装盲盒 活动时间：2021-2-26至2021-3-8
 5 0,7,23 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 
-#京东国际盲盒
+#京东国际盲盒活动时间】2021年02月23日 起至 2021年03月31日 18:00:00
 5 7,12,23 * * * node /scripts/jd_global_mh.js >> /scripts/logs/jd_global_mh.log 2>&1
-#京东极速版天天领红包 活动时间：2021-1-18至2021-3-3
-5 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
+
 ##############长期活动##############
 # 签到
 3 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
