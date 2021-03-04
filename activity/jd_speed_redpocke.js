96af49f028318b81d2ff66c181854e9be615f540
lxk0301 <778732665@qq.com>
2021-03-04 10:07:00 +0800

    update

--- activity/jd_speed_redpocke.js
+++ activity/jd_speed_redpocke.js
@@ -25,9 +25,9 @@ cron "0 0 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_
 
 const $ = new Env('京东极速版红包');
 
-const notify = $.isNode() ? require('./sendNotify') : '';
+const notify = $.isNode() ? require('../sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('../jdCookie.js') : '';
 
 
 let cookiesArr = [], cookie = '', message;
