dc356cf7ced1e5a567f3b073903c278e2133e25d
lxk0301 <778732665@qq.com>
2021-03-08 19:09:38 +0800

    update

--- jd_nzmh.js
+++ jd_nzmh.js
@@ -68,6 +68,7 @@ if ($.isNode()) {
         await jdMh()
         await jdMh('https://anmp.jd.com/babelDiy/Zeus/gY7ymUmC8ZM74Zw3woiDDQU1naT/index.html?wxAppName=jd')
         // await jdMh('https://anmp.jd.com/babelDiy/Zeus/3UGPT8RMBu4kL2YAYN98MgkcDhRq/index.html?wxAppName=jd')
+        await jdMh('https://anmp.jd.com/babelDiy/Zeus/yiNQjMxQvs3R3SdS4nwa2MFk1FE/index.html?wxAppName=jd')
       } catch (e) {
         $.logErr(e)
       }
