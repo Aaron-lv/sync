a455e8441a5aaa55ce6695eff8affedb9fb40c0c
lxk0301 <778732665@qq.com>
2021-03-08 15:56:43 +0800

    docker crontab_list.sh添加京小兑.宠汪汪赛跑添加赛跑互助功能.默认关闭,可设置环境变量开启

--- jd_unsubscribe.js
+++ jd_unsubscribe.js
@@ -298,7 +298,7 @@ function TotalBean() {
               return
             }
             if (data['retcode'] === 0) {
-              $.nickName = data['base'].nickname;
+              $.nickName = (data['base'] && data['base'].nickname) || $.UserName;
             } else {
               $.nickName = $.UserName
             }
