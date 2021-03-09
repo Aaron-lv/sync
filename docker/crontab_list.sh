4c02fa853cfab885662ecd02d3c8fa08f9dd1047
lxk0301 <778732665@qq.com>
2021-03-09 16:38:16 +0800

    update 超级直播间红包雨

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -11,6 +11,8 @@
 15 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 #女装盲盒 活动时间：2021-3-8至2021-3-20
 5 1,23 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
+#超级直播间红包雨
+30,31 20-23/1 9,12 3 * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
 ##############长期活动##############
 # 签到
 3 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
