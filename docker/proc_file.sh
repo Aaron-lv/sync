febb70a262a7768fabd7b90583265cdd99656bea
lxk0301 <778732665@qq.com>
2021-03-09 18:47:34 +0800

    proc_file.sh update
    浏览文件优化kill jd_crazy命令

--- docker/proc_file.sh
+++ docker/proc_file.sh
@@ -45,19 +45,19 @@ echo "整理完成，具体结果请查看${sharecodeFile}文件"
 echo "处理jd_crazy_joy_coin任务。。。"
 if [ ! $CRZAY_JOY_COIN_ENABLE ]; then
    echo "默认启用jd_crazy_joy_coin杀掉jd_crazy_joy_coin任务，并重启"
-   eval $(ps -ef | grep "jd_crazy" | awk '{print "kill "$1}')
+   eval $(ps -ef | grep "jd_crazy" | grep -v "grep" | awk '{print "kill "$1}')
    echo '' >/scripts/logs/jd_crazy_joy_coin.log
    node /scripts/jd_crazy_joy_coin.js | ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
    echo "默认jd_crazy_joy_coin重启完成"
 else
    if [ $CRZAY_JOY_COIN_ENABLE = "Y" ]; then
       echo "配置启用jd_crazy_joy_coin，杀掉jd_crazy_joy_coin任务，并重启"
-      eval $(ps -ef | grep "jd_crazy" | awk '{print "kill "$1}')
+      eval $(ps -ef | grep "jd_crazy" | grep -v "grep" | awk '{print "kill "$1}')
       echo '' >/scripts/logs/jd_crazy_joy_coin.log
       node /scripts/jd_crazy_joy_coin.js | ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
       echo "配置jd_crazy_joy_coin重启完成"
    else
-      eval $(ps -ef | grep "jd_crazy" | awk '{print "kill "$1}')
+      eval $(ps -ef | grep "jd_crazy" | grep -v "grep" | awk '{print "kill "$1}')
       echo "已配置不启用jd_crazy_joy_coin任务，不处理"
    fi
 fi
