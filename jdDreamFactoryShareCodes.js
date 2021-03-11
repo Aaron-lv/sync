dd20414854dd80e36b91d03cdb4aed2ae5baf716
lxk0301 <778732665@qq.com>
2021-03-11 10:26:32 +0800

    fix

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -17,7 +17,7 @@ let shareCodes = [
 //   process.env.DREAM_FACTORY_SHARE_CODES = logShareCodes.DREAM_FACTORY_SHARE_CODES.join('&');
 // }
 
-// 判断github action里面是否有京喜工厂互助码
+// 判断环境变量里面是否有京喜工厂互助码
 if (process.env.DREAM_FACTORY_SHARE_CODES) {
   if (process.env.DREAM_FACTORY_SHARE_CODES.indexOf('&') > -1) {
     console.log(`您的互助码选择的是用&隔开\n`)
@@ -28,7 +28,7 @@ if (process.env.DREAM_FACTORY_SHARE_CODES) {
   } else {
     shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split();
   }
-} else if (process.env.DREAM_FACTORY_SHARE_CODES) {
+} else {
   console.log(`由于您环境变量(DREAM_FACTORY_SHARE_CODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
 }
 for (let i = 0; i < shareCodes.length; i++) {
