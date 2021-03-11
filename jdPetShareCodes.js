55c7239585085d3727e2854473df89c2de1b81f5
lxk0301 <778732665@qq.com>
2021-03-11 10:26:32 +0800

    fix

--- jdPetShareCodes.js
+++ jdPetShareCodes.js
@@ -28,7 +28,7 @@ if (process.env.PETSHARECODES) {
   } else {
     PetShareCodes = process.env.PETSHARECODES.split();
   }
-} else if (process.env.JD_COOKIE) {
+} else {
   console.log(`由于您环境变量(PETSHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
 }
 for (let i = 0; i < PetShareCodes.length; i++) {
