4a3502f0bc8fb71e2c83cb00c3dce8a94b7aafa8
lxk0301 <778732665@qq.com>
2021-03-11 10:57:19 +0800

    del

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,16 +5,13 @@
 
 ##############短期活动##############
 
-#京东国际盲盒活动时间】2021年02月23日 起至 2021年03月31日 18:00:00
-5 7,12,23 * * * node /scripts/jd_global_mh.js >> /scripts/logs/jd_global_mh.log 2>&1
-#环球挑战赛 第二季(活动时间：2021-03-08 至 2021-03-31)
-35 6,22 * * * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
 #京东极速版红包(活动时间：2021-3-8至2021-3-25)
 15 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 #女装盲盒 活动时间：2021-3-8至2021-3-20
 5 1,23 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 #超级直播间红包雨
 30,31 20-23/1 9,12 3 * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
+
 ##############长期活动##############
 # 签到
 3 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
