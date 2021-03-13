2e97ea254f9824c4871dc963708051201068d4cb
lxk0301 <778732665@qq.com>
2021-03-13 13:41:33 +0800

    jd_global.js jd_global_mh.js jd_global.js crontab_list.sh update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -11,6 +11,11 @@
 5 1,23 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 #超级直播间红包雨(未知，出现异常提示请忽略。红包雨期间会正常)
 30,31 20-23/1 * * * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
+#京东国际盲盒活动时间】2021年02月23日 起至 2021年03月31日 18:00:00
+5 7,12,23 * * * node /scripts/jd_global_mh.js >> /scripts/logs/jd_global_mh.log 2>&1
+#环球挑战赛 第二季(活动时间：2021-03-08 至 2021-03-31)
+35 6,22 * * * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
+
 
 ##############长期活动##############
 # 签到
