bafd452b2073af2ebc70c58bfab594f850322abc
lxk0301 <778732665@qq.com>
2021-03-16 16:05:01 +0800

    update jdCookie.js.

--- jdCookie.js
+++ jdCookie.js
@@ -9,10 +9,8 @@ let CookieJDs = [
 // 判断环境变量里面是否有京东ck
 if (process.env.JD_COOKIE) {
   if (process.env.JD_COOKIE.indexOf('&') > -1) {
-    console.log(`您的cookie选择的是用&隔开\n`)
     CookieJDs = process.env.JD_COOKIE.split('&');
   } else if (process.env.JD_COOKIE.indexOf('\n') > -1) {
-    console.log(`您的cookie选择的是用换行隔开\n`)
     CookieJDs = process.env.JD_COOKIE.split('\n');
   } else {
     CookieJDs = [process.env.JD_COOKIE];
@@ -26,10 +24,11 @@ if (JSON.stringify(process.env).indexOf('GITHUB')>-1) {
   })()
 }
 CookieJDs = [...new Set(CookieJDs.filter(item => !!item))]
-console.log(`\n====================共有${CookieJDs.length}个京东账号Cookie=========\n`);
+console.log(`\n====================共${CookieJDs.length}个京东账号Cookie=========\n`);
 console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
 if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
 for (let i = 0; i < CookieJDs.length; i++) {
+  if (!CookieJDs[i].match(/pt_pin=(.+?);/) || !CookieJDs[i].match(/pt_key=(.+?);/)) console.log(`\n提示:京东cookie 【${CookieJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: pt_key=xxx;pt_pin=xxx;（分号;不可少）\n`);
   const index = (i + 1 === 1) ? '' : (i + 1);
   exports['CookieJD' + index] = CookieJDs[i].trim();
 }
