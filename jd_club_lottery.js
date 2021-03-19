d4d0028a552940924d397e978772fb9e14a044c0
lxk0301 <778732665@qq.com>
2021-03-19 14:00:10 +0800

    移除 jd_joy_steal.js脚本，app和小程序都需进行拼图验证

--- jd_club_lottery.js
+++ jd_club_lottery.js
@@ -67,7 +67,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
         continue
       }
       await clubLottery();
-      //await shakeSign();
+      // await shakeSign();
       await showMsg();
     }
   }
@@ -354,17 +354,23 @@ async function shakeSign() {
   let body = {"floorToken": "df6e1996-0f6f-4c29-92b9-22416fc1f68a", "dataSourceCode": "popUpInfo", "argMap": {}};
   const res = await pg_interact_interface_invoke(body);
   if (res.success && res.data) {
-    if (res['data']['popUpRemainTimes'] > 0) {
-      //可签到
-      body = {"floorToken":"f1d574ec-b1e9-43ba-aa84-b7a757f27f0e","dataSourceCode":"signIn","argMap":{"currSignCursor": res['data']['dayBeanAmount']}}
-      const signRes = await pg_interact_interface_invoke(body);
-      console.log(`京东会员第${res['data']['dayBeanAmount']}天签到结果；${JSON.stringify(signRes)}`)
-      if (signRes.success && signRes['data']) {
-        console.log(`京东会员第${res['data']['dayBeanAmount']}天签到成功。获得${signRes['data']['rewardVos'] && signRes['data']['rewardVos'][0]['jingBeanVo'] && signRes['data']['rewardVos'][0]['jingBeanVo']['beanNum']}京豆\n`)
-      }
-    } else {
-      console.log(`京东会员第${res['data']['dayBeanAmount'] - 1}天已经签到了`)
+    body = {"floorToken":"f1d574ec-b1e9-43ba-aa84-b7a757f27f0e","dataSourceCode":"signIn","argMap":{"currSignCursor": res['data']['dayBeanAmount']}}
+    const signRes = await pg_interact_interface_invoke(body);
+    console.log(`京东会员第${res['data']['dayBeanAmount']}天签到结果；${JSON.stringify(signRes)}`)
+    if (signRes.success && signRes['data']) {
+      console.log(`京东会员第${res['data']['dayBeanAmount']}天签到成功。获得${signRes['data']['rewardVos'] && signRes['data']['rewardVos'][0]['jingBeanVo'] && signRes['data']['rewardVos'][0]['jingBeanVo']['beanNum']}京豆\n`)
     }
+    // if (res['data']['popUpRemainTimes'] > 0) {
+    //   //可签到
+    //   body = {"floorToken":"f1d574ec-b1e9-43ba-aa84-b7a757f27f0e","dataSourceCode":"signIn","argMap":{"currSignCursor": res['data']['dayBeanAmount']}}
+    //   const signRes = await pg_interact_interface_invoke(body);
+    //   console.log(`京东会员第${res['data']['dayBeanAmount']}天签到结果；${JSON.stringify(signRes)}`)
+    //   if (signRes.success && signRes['data']) {
+    //     console.log(`京东会员第${res['data']['dayBeanAmount']}天签到成功。获得${signRes['data']['rewardVos'] && signRes['data']['rewardVos'][0]['jingBeanVo'] && signRes['data']['rewardVos'][0]['jingBeanVo']['beanNum']}京豆\n`)
+    //   }
+    // } else {
+    //   console.log(`京东会员第${res['data']['dayBeanAmount'] - 1}天已经签到了`)
+    // }
   }
 }
 function pg_interact_interface_invoke(body) {
