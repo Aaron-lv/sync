e1ccd7f0a463c625253aa6449b30c4ea2efdd04f
lxk0301 <778732665@qq.com>
2021-03-20 08:28:43 +0800

    crontab_list.sh update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -15,8 +15,7 @@
 35 7,12,23 * * * node /scripts/jd_global_mh.js >> /scripts/logs/jd_global_mh.log 2>&1
 #环球挑战赛 第二季(活动时间：2021-03-08 至 2021-03-31)
 5 6,22 * * * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
-#手机尚学季 活动时间：2021年3月15日 00:00:00-2021年3月19日 23:59:59
-0 0,12 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1
+
 #5G超级盲盒 活动时间：2021-03-19到2021-04-30
 0 0,1-23/4 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
 
