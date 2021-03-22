ede2f74a95bcb16245c8be9cfd0ad4ce3ee5dd26
lxk0301 <778732665@qq.com>
2021-03-22 13:16:21 +0800

    女装盲盒 活动时间：2021-3-22至2021-3-31 活动地址：https://anmp.jd.com/babelDiy/Zeus/3gpAsWd6UBb1MWvr6PFYjNS4Nexk/index.html 活动入口：京东app-女装馆-赢京豆

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -7,7 +7,7 @@
 
 #京东极速版红包(活动时间：2021-3-8至2021-3-25)
 45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
-#女装盲盒 活动时间：2021-3-8至2021-3-20
+#女装盲盒 活动时间：2021-03-22到2021-03-31
 35 1,23 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 #超级直播间红包雨(活动时间不定期，出现异常提示请忽略。红包雨期间会正常)
 30,31 20-23/1 * * * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
@@ -18,8 +18,6 @@
 
 #5G超级盲盒 活动时间：2021-03-19到2021-04-30
 10 0,1-23/4 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
-#女装盲盒 活动时间：2021-03-22到2021-03-31
-10 0,23 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 
 40 0-23/1 * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
 ##############长期活动##############
