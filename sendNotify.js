426a139b9a09f7a180f16cacbeab22c04f0fd992
lxk0301 <778732665@qq.com>
2021-03-26 12:19:53 +0800

    sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -137,7 +137,7 @@ if (process.env.PUSH_PLUS_USER) {
 
 async function sendNotify(text, desp, params = {}) {
   //提供6种通知
-  desp += `\n\n本脚本开源免费使用 By：https://gitee.com/lxk0301/jd_docker`;
+  desp += `\n\n本脚本免费使用 By：https://gitee.com/lxk0301/jd_docker`;
   await Promise.all([
     serverNotify(text, desp),//微信server酱
     pushPlusNotify(text, desp)//pushplus(推送加)
