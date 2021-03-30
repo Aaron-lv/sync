57070ec5eda2641645284f09a3d676c0abb51fd9
lxk0301 <778732665@qq.com>
2021-03-31 00:05:51 +0800

    crontab_list.sh update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -49,7 +49,7 @@
 # 京东天天加速
 28 */3 * * * node /scripts/jd_speed.js >> /scripts/logs/jd_speed.log 2>&1
 # 东东超市
-31 1-23/5 * * * node /scripts/jd_superMarket.js >> /scripts/logs/jd_superMarket.log 2>&1
+31 0,1-23/2 * * * node /scripts/jd_superMarket.js >> /scripts/logs/jd_superMarket.log 2>&1
 # 取关京东店铺商品
 45 23 * * * node /scripts/jd_unsubscribe.js >> /scripts/logs/jd_unsubscribe.log 2>&1
 # 京豆变动通知
