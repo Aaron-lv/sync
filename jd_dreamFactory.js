b796b69974fad8e07168db3900296261de0d99d4
lxk0301 <778732665@qq.com>
2021-05-16 08:11:41 +0800

    修复报错

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -737,7 +737,7 @@ function DrawProductionStagePrize() {
 async function PickUp(encryptPin = $.encryptPin, help = false) {
   $.pickUpMyselfComponent = true;
   const GetUserComponentRes = await GetUserComponent(encryptPin, 1500);
-  if (GetUserComponentRes && GetUserComponentRes['ret'] === 0) {
+  if (GetUserComponentRes && GetUserComponentRes['ret'] === 0 && GetUserComponentRes['data']) {
     const { componentList } = GetUserComponentRes['data'];
     if (componentList && componentList.length <= 0) {
       if (help) {
