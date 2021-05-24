8c2b0448cb6695beaa5395930b355b5b1cd56927
wisz2021 <wei09mozzie@gmail.com>
2021-05-24 10:12:37 +0800

    fix

--- jd_carnivalcity.js
+++ jd_carnivalcity.js
@@ -53,7 +53,7 @@ if ($.isNode()) {
 }
 let inviteCodes = [];
 const JD_API_HOST = 'https://carnivalcity.m.jd.com';
-const activeEndTime = '2021/4/21 00:00:00+08:00';
+const activeEndTime = '2021/06/20 00:00:00+08:00';
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
