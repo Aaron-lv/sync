191df1fdae2cac630a9db356260a068efbcb51bb
wisz2021 <wei09mozzie@gmail.com>
2021-05-24 09:41:42 +0800

    手机狂欢城,还有一丢丢问题,先更新上去

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -17,6 +17,9 @@
 13 1,22,23 * * * node /scripts/jd_daily_lottery.js >> /scripts/logs/jd_daily_lottery.log 2>&1
 #城城领现金
 33 * * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
+
+#手机狂欢城
+0 0,12,18,21 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
