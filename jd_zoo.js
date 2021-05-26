8845b93b83ff8959e4cfea992c4cbcf99f8cc51f
DoveBoy <393366046@qq.com>
2021-05-26 18:59:55 +0800

    update

--- jd_zoo.js
+++ jd_zoo.js
@@ -34,7 +34,9 @@ const pKHelpAuthorFlag = true;//是否助力作者PK  true 助力，false 不助
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [];
 $.cookie = '';
-$.inviteList = [];
+$.inviteList = [
+  'ZXTKT0225KkcRB8c_VODck-nl_8IdgFjRWn6-7zx55awQ',
+];
 $.pkInviteList = [];
 $.secretpInfo = {};
 $.innerPkInviteList = [
