a7d08d35e9d8ed0ed99bce07f8b6ac44348f1954
lxk0301 <778732665@qq.com>
2021-05-26 13:20:27 +0800

    default_task.sh

--- docker/default_task.sh
+++ docker/default_task.sh
@@ -247,6 +247,6 @@ echo "第11步将仓库的docker_entrypoint.sh脚本更新至系统/usr/local/bi
 cat /scripts/docker/docker_entrypoint.sh >/usr/local/bin/docker_entrypoint.sh
 
 echo "发送通知"
-export NOTIFY_CONTENT="升级容器内部node版本(v14.5.0)"
+export NOTIFY_CONTENT=""
 cd /scripts/docker
 node notify_docker_user.js
