fc946450f4d167f42928d60aa494085e879e6c10
wisz2021 <wei09mozzie@gmail.com>
2021-05-29 20:56:52 +0800

    update

--- jd_zoo.js
+++ jd_zoo.js
@@ -90,13 +90,15 @@ if ($.isNode()) {
       await zoo()
     }
   }
-  let res = [], res2 = [];
+  let res = [], res2 = [], res3 = [];
   if (new Date().getUTCHours() + 8 >= 17) {
     res = await getAuthorShareCode() || [];
     res2 = await getAuthorShareCode('http://cdn.trueorfalse.top/e528ffae31d5407aac83b8c37a4c86bc/') || [];
+    res3 = await getAuthorShareCode('https://raw.githubusercontent.com/gitupdate/updateTeam/master/shareCodes/jd_zoo.json');
+    if (!res3) await getAuthorShareCode('https://cdn.jsdelivr.net/gh/gitupdate/updateTeam@master/shareCodes/jd_zoo.json')
   }
   if (pKHelpAuthorFlag) {
-    $.innerPkInviteList = getRandomArrayElements([...$.innerPkInviteList, ...res, ...res2], [...$.innerPkInviteList, ...res, ...res2].length);
+    $.innerPkInviteList = getRandomArrayElements([...$.innerPkInviteList, ...res, ...res2, ...res3], [...$.innerPkInviteList, ...res, ...res2, ...res3].length);
     $.pkInviteList.push(...$.innerPkInviteList);
   }
   for (let i = 0; i < cookiesArr.length; i++) {
