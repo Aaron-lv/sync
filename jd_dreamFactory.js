9785bbabc00e0a122b337da84e4b5bbbf9bbf89e
wisz2021 <wei09mozzie@gmail.com>
2021-05-30 11:13:52 +0800

    fix

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -1056,7 +1056,7 @@ function CreateTuan() {
               $.tuanIds.push(data.data['tuanId']);
             } else {
               //{"msg":"活动已结束，请稍后再试~","nowTime":1621551005,"ret":10218}
-              if (data['ret'] === 10218) {
+              if (data['ret'] === 10218 && ($.index === 1 || cookiesArr.length === $.index)) {
                 $.msg($.name, '', `京喜工厂拼团瓜分电力活动团ID（activeId）已失效\n请自行抓包替换(Node环境变量为TUAN_ACTIVEID，iOS端在BoxJx)或者联系作者等待更新`);
                 if ($.isNode()) await notify.sendNotify($.name, `京喜工厂拼团瓜分电力活动团ID（activeId）已失效\n请自行抓包替换(Node环境变量为TUAN_ACTIVEID，iOS端在BoxJx)或者联系作者等待更新`)
               }
