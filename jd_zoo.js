66f82e672d8cf6c30f2937e8b5d3e56715f94691
DoveBoy <393366046@qq.com>
2021-06-02 08:09:57 +0800

    update

--- jd_zoo.js
+++ jd_zoo.js
@@ -36,7 +36,7 @@ let cookiesArr = [];
 $.cookie = '';
 $.inviteList = [];
 $.pkInviteList = [
-  'sSKNX-MpqKOJsNu8mJ7RA9BJMup4tAAmPcPPPhBUWYKUJ19UKeC8EAoKeUXELi4o',
+  'sSKNX-MpqKOJsNu8mJ7RA9BJMup4tAAmPcPPPhBUWYKUJ19UKeC8EAoKeUXELi4r',
 ];
 $.secretpInfo = {};
 $.innerPkInviteList = [
