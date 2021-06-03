50ce2d9bba63d0b0d1e8b39d615dc6cd0ff5b202
lxk0301 <12345677@qq.com>
2021-06-04 00:34:19 +0800

    crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -15,8 +15,6 @@
 
 #每日抽奖(活动时间：2021-05-01至2021-05-31)
 13 1,22,23 * * * node /scripts/jd_daily_lottery.js >> /scripts/logs/jd_daily_lottery.log 2>&1
-#城城领现金
-33 * * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
 
 #手机狂欢城
 0 0,12,18,21 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
