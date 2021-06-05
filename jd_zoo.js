e510847a82ebf3ac00547c58f88e86008deb4b87
DoveBoy <393366046@qq.com>
2021-06-05 09:03:26 +0800

    Update

--- jd_zoo.js
+++ jd_zoo.js
@@ -35,7 +35,9 @@ const pKHelpAuthorFlag = true;//是否助力作者PK  true 助力，false 不助
 let cookiesArr = [];
 $.cookie = '';
 $.inviteList = [];
-$.pkInviteList = [];
+$.pkInviteList = [
+  "sSKNX-MpqKOJsNu8mJ7RA9BJMup4tAAmPcPPPhBUWYKUJ19UKeC8EAoKeUXELi4s",
+];
 $.secretpInfo = {};
 $.innerPkInviteList = [
 ];
