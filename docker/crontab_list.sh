c6918974876b8f4ae04da10edb9890b06c72c0b7
wisz2021 <wei09mozzie@gmail.com>
2021-06-05 20:35:44 +0800

    省钱大赢家之翻翻乐

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -36,6 +36,8 @@
 20 1,21 * * * node /scripts/jd_mcxhd.js >> /scripts/logs/jd_mcxhd.log 2>&1
 #京喜领88元红包(6.31到期)
 30 1,6,12,21 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
+#省钱大赢家之翻翻乐
+10,40 * * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
 ##############长期活动##############
 # 签到
 7 0,17 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
