7e2b14404b90e9f88b2c1ab0d3748462ba86e7e5
star <jiayouLSH26!?>
2021-06-05 09:52:58 +0800

    update

--- jd_zoo.js
+++ jd_zoo.js
@@ -42,8 +42,6 @@ $.innerPkInviteList = [
   "sSKNX-MpqKOJsNu8mJ7RA9BJMup4tAAmPcPPPhBUWYKUJ19UKeC8EAoKeUXELi4s",
   'sSKNX-MpqKPS7Le4m5rbBpODDLhoZ9ruJViTqJpv4c2Lm2-TfJwzRBS82zBEzkXI',
   'sSKNX-MpqKOJsNvSzMSZfAM9H7GwE_7GAGP6h5-yWMFC6rsV_bSQHlBmw28ArA',
-  'sSKNX-MpqKOJsNvM-u2vdyHH5x3rrYdCxJ-sE_oy8C69xCinRiEdQ3bib4w42A',
-  'sSKNX-MpqKOJsNu-ms-IVdCm3z528hK5o2UJgQWu0nCM-DsOo6H6pwAnkox1F2YM'
 ];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
