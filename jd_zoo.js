38927d28a7aa368098344498457060fd5624af87
star <869728223@qq.com>
2021-06-07 11:27:52 +0800

    update

--- jd_zoo.js
+++ jd_zoo.js
@@ -92,7 +92,7 @@ if ($.isNode()) {
     res = await getAuthorShareCode() || [];
     res2 = await getAuthorShareCode('http://cdn.trueorfalse.top/e528ffae31d5407aac83b8c37a4c86bc/') || [];
   }
-  if (new Date().getHours() === 10 || (new Date().getHours() === 11 && new Date().getMinutes() < 35)) {
+  if (new Date().getHours() === 11 || new Date().getHours() === 12 || (new Date().getHours() === 13 && new Date().getMinutes() < 35)) {
     $.innerPkInviteList.push('sSKNX-MpqKPS7Le4m5rbBpODDLhoZ9ruJViTqJpv4c2Lm2-TfJwzRBS82zBEzkXK');
     $.innerPkInviteList.push('sSKNX-MpqKOJsNvSzMSZfAM9H7GwE_7GAGP6h5-yWMFC6rsV_bSQHlBmw28Arg');
     $.innerPkInviteList.push('sSKNX-MpqKOJsNvM-u2vdyHH5x3rrYdCxJ-sE_oy8C69xCinRiEdQ3bib4w42g');
