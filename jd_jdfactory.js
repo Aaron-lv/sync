d768b40ba31aea7bcc2ec9f5d6a04b577cf1d6e7
DoveBoy <393366046@qq.com>
2021-06-09 06:58:06 +0800

    Update

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -44,7 +44,8 @@ if ($.isNode()) {
 }
 let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = [`P04z54XCjVWnYaS5u2ak7ZCdan1Bdd2GGiWvC6_uERj`, 'P04z54XCjVWnYaS5m9cZ2ariXVJwHf0bgkG7Uo'];
+const inviteCodes = [`P04z54XCjVWnYaS5u2ak7ZCdan1Bdd2GGiWvC6_uERj`, 'P04z54XCjVWnYaS5m9cZ2ariXVJwHf0bgkG7Uo',
+`T0225KkcRB8c_VODck-nl_8IdgCjVWnYaS5kRrbA`];
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
