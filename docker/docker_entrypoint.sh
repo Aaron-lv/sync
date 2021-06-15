1695009dac7c624b741b549ef3be53835d5daa1d
裕湫林 <44421580+zyipk@users.noreply.github.com>
2021-06-15 12:40:30 +0800

    添加分支选择
    添加分支选择，以免以此仓库建立的docker镜像，后续无法自动更新仓库脚本

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -15,7 +15,7 @@ else
   git remote set-url origin "$REPO_URL"
   git reset --hard
   echo "git pull拉取最新代码..."
-  git -C /scripts pull --rebase
+  git -C /scripts pull origin "$REPO_BRANCH" --rebase
   echo "npm install 安装最新依赖"
   npm install --prefix /scripts
 fi
