4063227de1e86433e4f55ea53b92dc235c14115c
Aaron-lv <674560606@qq.com>
2021-07-14 11:21:59 +0800

    更新输出

--- jd_cfd.js
+++ jd_cfd.js
@@ -379,7 +379,7 @@ function helpdraw(dwUserId) {
         } else {
           data = JSON.parse(data);
           if (data.iRet === 0 || data.sErrMsg === "success") {
-            console.log(`领取助力奖励成功：获得${data.Data.ddwCoin}金币`)
+            console.log(`领取助力奖励成功：获得${data.Data.ddwCoin}金币 ${data.Data.StagePrizeInfo.ddwMoney}财富 ${data.Data.StagePrizeInfo.strPrizeName}红包`)
           } else {
             console.log(`领取助力奖励失败：${data.sErrMsg}`)
           }
