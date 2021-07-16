a4f05d82d95644f24abc7172159f3fc833256de6
Aaron-lv <674560606@qq.com>
2021-07-16 15:50:51 +0800

    更新查询用户信息接口

--- JD_DailyBonus.js
+++ JD_DailyBonus.js
@@ -1501,7 +1501,7 @@ function TotalBean() {
   return new Promise(resolve => {
     if (disable("Qbear")) return resolve()
     $nobyda.get({
-      url: 'https://wq.jd.com/user_new/info/GetJDUserInfoUnion',
+      url: 'https://wq.jd.com/user_new/info/GetJDUserInfoUnion?sceneval=2',
       headers: {
         Cookie: KEY
       }
