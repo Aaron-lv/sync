26cfeca832236f64511912ac70207ac179cd8e2c
Ca11back <Steliske@gmail.com>
2021-07-20 09:25:24 +0800

    增加适配12点场

--- jd_cfdtx.js
+++ jd_cfdtx.js
@@ -1,6 +1,6 @@
 /*
 京喜财富岛提现
-cron 59 23 * * * jd_cfdtx.js
+cron 59 23,11 * * * jd_cfdtx.js
 更新时间：2021-7-20
 活动入口：京喜APP-我的-京喜财富岛提现
 
@@ -9,14 +9,14 @@ cron 59 23 * * * jd_cfdtx.js
 ============Quantumultx===============
 [task_local]
 #京喜财富岛提现
-59 23 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js, tag=京喜财富岛提现, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxcfd.png, enabled=true
+59 23,11 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js, tag=京喜财富岛提现, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxcfd.png, enabled=true
 
 ================Loon==============
 [Script]
-cron "59 23 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js,tag=京喜财富岛提现
+cron "59 23,11 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js,tag=京喜财富岛提现
 
 ===============Surge=================
-京喜财富岛提现 = type=cron,cronexp="59 23 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js
+京喜财富岛提现 = type=cron,cronexp="59 23,11 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js
 
 ============小火箭=========
 京喜财富岛提现 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js, cronexpr="59 23 * * *", timeout=3600, enable=true
@@ -104,7 +104,7 @@ let nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 
 
 async function cfd() {
   try {
-    if (nowTimes.getHours() === 23 && nowTimes.getMinutes() === 59) {
+    if ((nowTimes.getHours() === 23 || nowTimes.getHours() === 11) && nowTimes.getMinutes() === 59) {
       let nowtime = new Date().Format("ss")
       let starttime = process.env.CFD_STARTTIME ? process.env.CFD_STARTTIME : 60;
       if(nowtime < 59) {
