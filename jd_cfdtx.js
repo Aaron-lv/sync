db774abea52a5c00c546f0d57f3a4cea49d21d43
Aaron-lv <674560606@qq.com>
2021-07-21 00:07:57 +0800

    修复时间判断

--- jd_cfdtx.js
+++ jd_cfdtx.js
@@ -36,7 +36,7 @@ $.showLog = $.getdata("cfd_showLog") ? $.getdata("cfd_showLog") === "true" : fal
 $.notifyTime = $.getdata("cfd_notifyTime");
 $.result = [];
 $.shareCodes = [];
-let cookiesArr = [], cookie = '', token, nowTimes;
+let cookiesArr = [], cookie = '', token;
 let allMessage = '', message = ''
 
 if ($.isNode()) {
@@ -103,8 +103,7 @@ Date.prototype.Format = function (fmt) { //author: meizz
 
 async function cfd() {
   try {
-    nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000)
-    if ((nowTimes.getHours() === 11 || nowTimes.getHours() === 23) && nowTimes.getMinutes() === 59) {
+    if ((new Date().getHours() === 11 || new Date().getHours() === 23) && new Date().getMinutes() === 59) {
       let nowtime = new Date().Format("ss")
       let starttime = process.env.CFD_STARTTIME ? process.env.CFD_STARTTIME : 60;
       if(nowtime < 59) {
@@ -169,9 +168,9 @@ async function userCashOutState(type = true) {
           if (type) {
             if (data.dwTodayIsCashOut !== 1) {
               if (data.ddwUsrTodayGetRich >= data.ddwTodayTargetUnLockRich) {
-                if (nowTimes.getHours() >= 0 && nowTimes.getHours() < 12) {
+                if (new Date().getHours() >= 0 && new Date().getHours() < 12) {
                   data.UsrCurrCashList = data.UsrCurrCashList.filter((x) => x.ddwMoney / 100 >= 1)
-                } else if (nowTimes.getHours() === 12 && nowTimes.getMinutes() <= 10) {
+                } else if (new Date().getHours() === 12 && new Date().getMinutes() <= 10) {
                   data.UsrCurrCashList = data.UsrCurrCashList.filter((x) => x.ddwMoney / 100 >= 0.5)
                 }
                 for (let key of Object.keys(data.UsrCurrCashList).reverse()) {
