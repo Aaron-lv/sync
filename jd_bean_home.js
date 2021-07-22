e07f7566e771ae6dff2081fb7657d65e2b929435
Aaron-lv <674560606@qq.com>
2021-07-22 22:36:53 +0800

    领京豆添加升级领京豆

--- jd_bean_home.js
+++ jd_bean_home.js
@@ -28,7 +28,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 const helpAuthor = true; // 是否帮助作者助力，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
-let cookiesArr = [], cookie = '', message;
+let cookiesArr = [], cookie = '', uuid = '', message;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item])
@@ -59,6 +59,7 @@ const JD_API_HOST = 'https://api.m.jd.com/';
       $.isLogin = true;
       $.nickName = '';
       message = '';
+      uuid = randomString(40)
       await TotalBean();
       console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
       if (!$.isLogin) {
@@ -142,10 +143,157 @@ async function jdBeanHome() {
     await $.wait(1000)
     await getTaskList();
     await receiveJd2();
+
+    await beanTaskList(1)
+    $.doneState = false
+    do {
+      await $.wait(2000)
+      await beanTaskList(2)
+    } while (!$.doneState)
+    await $.wait(2000)
+    if ($.doneState) await beanTaskList(3)
+
     await showMsg();
   } catch (e) {
     $.logErr(e)
   }
+}
+
+// 领京豆任务
+async function beanTaskList(type) {
+  return new Promise(resolve => {
+    $.post(taskBeanUrl('beanTaskList', {"viewChannel":"myjd"}), async (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} beanTaskList API请求失败，请检查网路重试`)
+        } else {
+          if (safeGet(data)) {
+            data = JSON.parse(data);
+            switch (type) {
+              case 1:
+                console.log(`当前等级:${data.data.curLevel} 下一级可领取:${data.data.nextLevelBeanNum || 0}京豆`)
+                if (!data.data.viewAppHome.takenTask) {
+                  console.log(`去做[${data.data.viewAppHome.mainTitle}]`)
+                  await beanHomeIconDoTask({"flag":"0","viewChannel":"myjd"})
+                }
+                await $.wait(2000)
+                if (!data.data.viewAppHome.doneTask) {
+                  console.log(`去领奖[${data.data.viewAppHome.mainTitle}]`)
+                  await beanHomeIconDoTask({"flag":"1","viewChannel":"AppHome"})
+                } else {
+                  console.log(`[${data.data.viewAppHome.mainTitle}]已做完`)
+                }
+                break
+              case 2:
+                $.doneState = true
+                let taskInfos = data.data.taskInfos
+                for (let key of Object.keys(taskInfos)) {
+                  let vo = taskInfos[key]
+                  if (vo.times < vo.maxTimes) {
+                    for (let key of Object.keys(vo.subTaskVOS)) {
+                      let taskList = vo.subTaskVOS[key]
+                      if (taskList.status === 1) {
+                        $.doneState = false
+                        console.log(`去做[${vo.taskName}]${taskList.title}`)
+                        await $.wait(1000)
+                        await beanDoTask({"actionType": 1, "taskToken": `${taskList.taskToken}`}, vo.taskType)
+                        if (vo.taskType === 9) {
+                          await $.wait(3000)
+                          await beanDoTask({"actionType": 0, "taskToken": `${taskList.taskToken}`}, vo.taskType)
+                        }
+                      }
+                    }
+                  }
+                }
+                break
+              case 3:
+                let taskInfos3 = data.data.taskInfos
+                for (let key of Object.keys(taskInfos3)) {
+                  let vo = taskInfos3[key]
+                  if (vo.times === vo.maxTimes) {
+                    console.log(`[${vo.taskName}]已做完`)
+                  }
+                }
+              default:
+                break
+            }
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+function beanDoTask(body, taskType) {
+  return new Promise(resolve => {
+    $.post(taskBeanUrl('beanDoTask', body), (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} beanDoTask API请求失败，请检查网路重试`)
+        } else {
+          if (safeGet(data)) {
+            data = JSON.parse(data);
+            if (body.actionType === 1 && taskType !== 9) {
+              if (data.code === "0" && data.data.bizCode === "0") {
+                console.log(`完成任务，获得+${data.data.score}成长值`)
+              } else {
+                console.log(`完成任务失败：${data}`)
+              }
+            }
+            if (body.actionType === 0) {
+              if (data.code === "0" && data.data.bizCode === "0") {
+                console.log(data.data.bizMsg)
+              } else {
+                console.log(`完成任务失败：${data}`)
+              }
+            } 
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve(data);
+      }
+    })
+  })
+}
+function beanHomeIconDoTask(body) {
+  return new Promise(resolve => {
+    $.post(taskBeanUrl('beanHomeIconDoTask', body), (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} beanHomeIconDoTask API请求失败，请检查网路重试`)
+        } else {
+          if (safeGet(data)) {
+            data = JSON.parse(data);
+            if (body.flag === "0" && data.data.taskResult) {
+              console.log(data.data.remindMsg)
+            }
+            if (body.flag === "1" && data.data.taskResult) {
+              console.log(data.data.remindMsg)
+            }
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve(data);
+      }
+    })
+  })
+}
+function randomString(e) {
+  e = e || 32;
+  let t = "abcdefhijkmnprstwxyz2345678", a = t.length, n = "";
+  for (i = 0; i < e; i++)
+    n += t.charAt(Math.floor(Math.random() * a));
+  return n
 }
 
 function getRandomInt(min, max) {
@@ -165,7 +313,7 @@ function doTask2() {
             if (safeGet(data)) {
               data = JSON.parse(data);
               if (data.code === '0' && data.data){
-                console.log(`任务完成进度：${data.data.taskProgress} / ${data.data.taskThreshold}`)
+                console.log(`任务完成进度：${data.data.taskProgress}/${data.data.taskThreshold}`)
                 if(data.data.taskProgress === data.data.taskThreshold)
                   $.doneState = true
               } else if (data.code === '0' && data.errorCode === 'HT201') {
@@ -486,6 +634,21 @@ function taskGetUrl(function_id, body) {
   }
 }
 
+function taskBeanUrl(function_id, body = {}) {
+  return {
+    url: `${JD_API_HOST}client.action?functionId=${function_id}&body=${escape(JSON.stringify(body))}&appid=ld&client=apple&clientVersion=10.0.8&uuid=${uuid}&openudid=${uuid}`,
+    headers: {
+      'Cookie': cookie,
+      'Host': 'api.m.jd.com',
+      'Accept': '*/*',
+      'Connection': 'keep-alive',
+      'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
+      'Accept-Language': 'zh-cn',
+      'Accept-Encoding': 'gzip, deflate, br',
+      "Referer": "https://h5.m.jd.com/"
+    }
+  }
+}
 
 function taskUrl(function_id, body) {
   body["version"] = "9.0.0.1";
