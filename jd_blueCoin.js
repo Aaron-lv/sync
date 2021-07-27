ea2179c4bcf77deba91eb49fd97b7ad9aadb6dcd
Aaron-lv <674560606@qq.com>
2021-07-27 09:05:08 +0800

    蓝币兑换增加火爆退出

--- jd_blueCoin.js
+++ jd_blueCoin.js
@@ -361,7 +361,7 @@ function smtg_obtainPrize(prizeId, timeout = 0, functionId = 'smt_exchangePrize'
             if ($.data.data.bizCode === 400) {
               $.errBizCodeCount ++;
               console.log(`debug 兑换京豆活动火爆次数:${$.errBizCodeCount}`);
-              if ($.errBizCodeCount >= 1) return
+              if ($.errBizCodeCount >= 3) return
             }
             if ($.data.data.bizCode === 0) {
               if (`${coinToBeans}` === '1000') {
