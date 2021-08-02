337b834eaf78cf9367ec8a9466a90bf554bf9d15
Aaron-lv <674560606@qq.com>
2021-08-02 23:28:29 +0800

    修复手残导致的错误

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -742,7 +742,7 @@ function TotalBean() {
         Host: "me-api.jd.com",
         Accept: "*/*",
         Connection: "keep-alive",
-        Cookie: cookie,
+        Cookie: $.cookie,
         "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
         "Accept-Language": "zh-cn",
         "Referer": "https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&",
