c0f9f923ea350d7c0e1df7351faf22bb4a015005
Aaron-lv <674560606@qq.com>
2021-08-03 18:33:42 +0800

    修复健康社区关注任务

--- jd_health.js
+++ jd_health.js
@@ -144,24 +144,35 @@ function getTaskDetail(taskId = '') {
               await doTask(data?.data?.result?.taskVos[0].shoppingActivityVos[0]?.taskToken, 22, 1)//领取任务
               await $.wait(1000 * (data?.data?.result?.taskVos[0]?.waitDuration || 3));
               await doTask(data?.data?.result?.taskVos[0].shoppingActivityVos[0]?.taskToken, 22, 0);//完成任务
-            } else for (let vo of data?.data?.result?.taskVos.filter(vo => vo.taskType !== 19) ?? []) {
-              console.log(`${vo.taskName}任务，完成次数：${vo.times}/${vo.maxTimes}`)
-              for (let i = vo.times; i < vo.maxTimes; ++i) {
-                console.log(`去完成${vo.taskName}任务`)
-                if (vo.taskType === 13) {
-                  await doTask(vo.simpleRecordInfoVo?.taskToken, vo?.taskId)
-                } else if (vo.taskType === 8) {
-                  await doTask(vo.productInfoVos[i]?.taskToken, vo?.taskId, 1)
-                  await $.wait(1000 * 10)
-                  await doTask(vo.productInfoVos[i]?.taskToken, vo?.taskId, 0)
-                } else if (vo.taskType === 9) {
-                  await doTask(vo.shoppingActivityVos[0]?.taskToken, vo?.taskId, 1)
-                  await $.wait(1000 * 10)
-                  await doTask(vo.shoppingActivityVos[0]?.taskToken, vo?.taskId, 0)
-                } else if (vo.taskType === 10) {
-                  await doTask(vo.threeMealInfoVos[0]?.taskToken, vo?.taskId)
-                } else if (vo.taskType === 26 || vo.taskType === 3) {
-                  await doTask(vo.shoppingActivityVos[0]?.taskToken, vo?.taskId)
+            } else {
+              for (let vo of data?.data?.result?.taskVos.filter(vo => vo.taskType !== 19) ?? []) {
+                console.log(`${vo.taskName}任务，完成次数：${vo.times}/${vo.maxTimes}`)
+                for (let i = vo.times; i < vo.maxTimes; i++) {
+                  console.log(`去完成${vo.taskName}任务`)
+                  if (vo.taskType === 13) {
+                    await doTask(vo.simpleRecordInfoVo?.taskToken, vo?.taskId)
+                  } else if (vo.taskType === 8) {
+                    await doTask(vo.productInfoVos[i]?.taskToken, vo?.taskId, 1)
+                    await $.wait(1000 * 10)
+                    await doTask(vo.productInfoVos[i]?.taskToken, vo?.taskId, 0)
+                  } else if (vo.taskType === 9) {
+                    await doTask(vo.shoppingActivityVos[0]?.taskToken, vo?.taskId, 1)
+                    await $.wait(1000 * 10)
+                    await doTask(vo.shoppingActivityVos[0]?.taskToken, vo?.taskId, 0)
+                  } else if (vo.taskType === 10) {
+                    await doTask(vo.threeMealInfoVos[0]?.taskToken, vo?.taskId)
+                  } else if (vo.taskType === 26 || vo.taskType === 3) {
+                    await doTask(vo.shoppingActivityVos[0]?.taskToken, vo?.taskId)
+                  } else if (vo.taskType === 1) {
+                    for (let key of Object.keys(vo.followShopVo)) {
+                      let taskFollow = vo.followShopVo[key]
+                      if (taskFollow.status !== 2) {
+                        await doTask(taskFollow.taskToken, vo.taskId, 0)
+                        break
+                      }
+                    }
+                  }
+                  await $.wait(2000)
                 }
               }
             }
