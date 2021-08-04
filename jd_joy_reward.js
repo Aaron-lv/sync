470170b0a9bd143d9f1afbeade94385de2392f2a
Aaron-lv <674560606@qq.com>
2021-08-04 17:30:14 +0800

    更新

--- jd_joy_reward.js
+++ jd_joy_reward.js
@@ -96,15 +96,16 @@ Date.prototype.Format = function (fmt) { //author: meizz
       $.done();
     })
 
-
 async function joyReward() {
   try {
-    let nowtime = new Date().Format("s.S")
-    let starttime = process.env.JOY_STARTTIME ? process.env.JOY_STARTTIME : 60;
-    if(nowtime < 59) {
-      let sleeptime = (starttime - nowtime) * 1000;
-      console.log(`等待时间 ${sleeptime / 1000}`);
-      await zooFaker.sleep(sleeptime)
+    if (new Date().getMinutes() === 59) {
+      let nowtime = new Date().Format("s.S")
+      let starttime = process.env.JOY_STARTTIME ? process.env.JOY_STARTTIME : 60;
+      if(nowtime < 59) {
+        let sleeptime = (starttime - nowtime) * 1000;
+        console.log(`等待时间 ${sleeptime / 1000}`);
+        await zooFaker.sleep(sleeptime)
+      }
     }
     for (let j = 0; j <= 10; j++) {
       await getExchangeRewards();
