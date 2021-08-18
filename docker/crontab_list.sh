1f2b08f6d67e0d7d03c0b4f5c54fa905e36c9766
Aaron-lv <674560606@qq.com>
2021-08-18 12:31:22 +0000

    添加众筹许愿池(无毛)

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -28,6 +28,8 @@
 10 0,8 * * * node /scripts/jd_carnivalcity_help.js >> /scripts/logs/jd_carnivalcity_help.log 2>&1
 # 工业品抽奖机
 5 0 * * * node /scripts/jd_industryLottery.js >> /scripts/logs/jd_industryLottery.log 2>&1
+# 众筹许愿池
+20 0,20 * * * node /scripts/jd_wish.js >> /scripts/logs/jd_wish.log 2>&1
 
 ##############长期活动##############
 # 签到
