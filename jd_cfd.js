d47dc4024c45405be8b8414edc50669943d4daaf
Ca11back <Steliske@gmail.com>
2021-08-19 09:08:30 +0800

    尝试修复环境变量助力码读取,并放在互助助力码之前,并去重

--- jd_cfd.js
+++ jd_cfd.js
@@ -88,12 +88,13 @@ $.appId = 10028;
       $.info = {}
       UA = `jdpingou;iPhone;4.13.0;14.4.2;${randomString(40)};network/wifi;model/iPhone10,2;appBuild/100609;ADID/00000000-0000-0000-0000-000000000000;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`
       token = await getJxToken()
-      await shareCodesFormat()
       await cfd();
       await $.wait(2000);
       UAInfo[$.UserName] = UA
     }
   }
+  await shareCodesFormat()
+  $.shareCodes = [...new Set([...$.newShareCodes,...$.shareCodes])]
   for (let j = 0; j < cookiesArr.length; j++) {
     cookie = cookiesArr[j];
     $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
