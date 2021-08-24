5c0b721e15412ecf7f2106747a82650ec1006d2d
Aaron-lv <674560606@qq.com>
2021-08-24 10:18:43 +0000

    更改UA

--- getJDCookie.js
+++ getJDCookie.js
@@ -4,7 +4,7 @@
  */
 const $ = new Env('扫码获取京东cookie');
 // const JD_UA = require('./USER_AGENTS').USER_AGENT;
-const JD_UA = `Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.14.0 main%2F1.0 baiduboxapp/11.18.0.16 (Baidu; P2 13.3.1) NABar/0.0`;
+const JD_UA = `Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5 UCBrowser/13.4.2.1122`;
 // console.info('JD_UA', JD_UA)
 const qrcode = require('qrcode-terminal');
 let s_token, cookies, guid, lsid, lstoken, okl_token, token
