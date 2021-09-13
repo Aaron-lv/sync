c818c079f5fb91ffb73c9b7e00c1ee054e8c062d
Aaron-lv <674560606@qq.com>
2021-09-13 12:33:46 +0800

    更新定时

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -119,7 +119,7 @@
 # 家庭号
 # 10 6,7 * * * node /scripts/jd_family.js >> /scripts/logs/jd_family.log 2>&1
 # 京东直播（又回来了）
-30-50/5 12,23 * * * node /scripts/jd_live.js >> /scripts/logs/jd_live.log 2>&1
+10 12-14 * * * node /scripts/jd_live.js >> /scripts/logs/jd_live.log 2>&1
 # 京东健康社区
 20 0,6,22 * * * node /scripts/jd_health.js >> /scripts/logs/jd_health.log 2>&1
 # 京东健康社区收集健康能量
