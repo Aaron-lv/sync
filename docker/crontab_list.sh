0e386f9301baf467dc42557f365d6f501a06075a
Aaron-lv <674560606@qq.com>
2021-10-02 00:01:48 +0800

    迁移过期活动

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -28,8 +28,6 @@
 15 2,5 * * * node /scripts/jd_connoisseur.js >> /scripts/logs/jd_connoisseur.log 2>&1
 # 母婴-跳跳乐
 1 0,11,21 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
-# 手机狂欢城(2021-9-16至2021-10-1)
-0 0-18/6 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 # 东东世界
 25 2,10 * * * node /scripts/jd_ddworld.js >> /scripts/logs/jd_ddworld.log 2>&1
 # 京东小魔方
