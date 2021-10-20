b44743acf7b354f38002f3aba3dfe6e7e80f18d7
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-10-21 06:51:59 +0800

    修复sddd领取

--- jd_sendBeans.js
+++ jd_sendBeans.js
@@ -198,7 +198,7 @@ async function myReward(){
           let canReward = false
           for (let key of Object.keys(data.datas)) {
             let vo = data.datas[key]
-            if (vo.status === 3 && vo.type === 2) {
+            if (vo.status === 3 && vo.type === 1) {
               canReward = true
               $.rewardRecordId = vo.id
               await rewardBean()
