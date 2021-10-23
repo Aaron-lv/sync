2ee6f53d743f7399b6e50c3298e956488ba48900
Aaron-lv <674560606@qq.com>
2021-10-23 09:47:52 +0800

    手机狂欢城

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -34,6 +34,10 @@
 0 0-23/1 * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
 # 预售福利机
 21 0,2 * * * node /scripts/jd_ys.js >> /scripts/logs/jd_ys.log 2>&1
+# 手机狂欢城
+0 0,12,18,21 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
+# 手机狂欢城助力
+10 0,8 * * * node /scripts/jd_carnivalcity_help.js >> /scripts/logs/jd_carnivalcity_help.log 2>&1
 
 ##############长期活动##############
 # 签到
