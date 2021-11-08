a96dad92c872d4508b761dd8fc83bff2182e2315
Aaron-lv <674560606@qq.com>
2021-11-08 15:27:24 +0800

    更新

--- jd_wish.js
+++ jd_wish.js
@@ -161,14 +161,17 @@ async function healthyDay_getHomeData(type = true) {
                     console.log(`签到`)
                     await harmony_collectScore({"appId":appId,"taskToken":vo.simpleRecordInfoVo.taskToken,"taskId":vo.taskId,"actionType":"0"}, vo.taskType)
                   } else if (vo.taskType === 1) {
+                    $.complete = false;
                     for (let key of Object.keys(vo.followShopVo)) {
                       let followShopVo = vo.followShopVo[key]
                       if (followShopVo.status !== 2) {
                         console.log(`【${followShopVo.shopName}】${vo.subTitleName}`)
                         await harmony_collectScore({"appId":appId,"taskToken":followShopVo.taskToken,"taskId":vo.taskId,"actionType":"0"})
+                        if ($.complete) break;
                       }
                     }
                   } else if (vo.taskType === 8) {
+                    $.complete = false;
                     for (let key of Object.keys(vo.productInfoVos)) {
                       let productInfoVos = vo.productInfoVos[key]
                       if (productInfoVos.status !== 2) {
@@ -176,9 +179,11 @@ async function healthyDay_getHomeData(type = true) {
                         await harmony_collectScore({"appId":appId,"taskToken":productInfoVos.taskToken,"taskId":vo.taskId,"actionType":"1"})
                         await $.wait(vo.waitDuration * 1000)
                         await harmony_collectScore({"appId":appId,"taskToken":productInfoVos.taskToken,"taskId":vo.taskId,"actionType":"0"})
+                        if ($.complete) break;
                       }
                     }
                   } else if (vo.taskType === 9 || vo.taskType === 26) {
+                    $.complete = false;
                     for (let key of Object.keys(vo.shoppingActivityVos)) {
                       let shoppingActivityVos = vo.shoppingActivityVos[key]
                       if (shoppingActivityVos.status !== 2) {
@@ -188,6 +193,7 @@ async function healthyDay_getHomeData(type = true) {
                           await $.wait(vo.waitDuration * 1000)
                         }
                         await harmony_collectScore({"appId":appId,"taskToken":shoppingActivityVos.taskToken,"taskId":vo.taskId,"actionType":"0"})
+                        if ($.complete) break;
                       }
                     }
                   } else if (vo.taskType === 14) {
@@ -242,6 +248,7 @@ function harmony_collectScore(body = {}, taskType = '') {
                 if (data.data.bizCode === 103) $.delcode = true
               } else {
                 console.log(body.actionType === "0" ? `完成任务失败：${data.data.bizMsg}\n` : data.data.bizMsg)
+                if (data.data.bizMsg === "任务已完成") $.complete = true;
               }
             }
           }
@@ -264,15 +271,17 @@ function interact_template_getLotteryResult() {
         } else {
           if (safeGet(data)) {
             data = JSON.parse(data);
-            let userAwardsCacheDto = data && data.data && data.data.result && data.data.result.userAwardsCacheDto
+            let userAwardsCacheDto = data && data.data && data.data.result && data.data.result.userAwardsCacheDto;
             if (userAwardsCacheDto) {
               if (userAwardsCacheDto.type === 2) {
-                console.log(`抽中：${userAwardsCacheDto.jBeanAwardVo.quantity}${userAwardsCacheDto.jBeanAwardVo.ext || `京豆`}`)
+                console.log(`抽中：${userAwardsCacheDto.jBeanAwardVo.quantity}${userAwardsCacheDto.jBeanAwardVo.ext || `京豆`}`);
               } else if (userAwardsCacheDto.type === 0) {
                 console.log(`很遗憾未中奖~`)
+              } else if (userAwardsCacheDto.type === 1) {
+                console.log(`抽中：${userAwardsCacheDto.couponVo.prizeName}，金额${userAwardsCacheDto.couponVo.usageThreshold}-${userAwardsCacheDto.couponVo.quota}，使用时间${userAwardsCacheDto.couponVo.useTimeRange}`);
               } else {
-                console.log(JSON.stringify(data))
-                message += `抽中：${JSON.stringify(data)}\n`
+                console.log(`抽中：${JSON.stringify(data)}`);
+                message += `抽中：${JSON.stringify(data)}\n`;
               }
             } else {
               $.canLottery = false
